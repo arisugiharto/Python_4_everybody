28c5619aa51e64a417d7a35210253f50f5975627
Ari Sugiharto <rarisugiharto@gmail.com>
2022-04-25 23:01:18 +0700

    adding new file to github

---
+++ Week-2/Excercise/emaildb.py
@@ -0,0 +1,79 @@
+<<<<<<< HEAD
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('Drop table if exists counts')
+
+cur.execute('''
+            CREATE TABLE Counts(email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if(len(fname) < 1):
+    fname = 'mbox-short.txt'
+
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From '):
+        continue
+
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    row = cur.fetchone()
+
+    if row is None:
+        cur.execute('INSERT INTO Counts(email, count) VALUES(?, 1)', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+
+    conn.commit()
+
+sql = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+    
+for row in cur.execute(sql):
+    print(str(row[0]), row[1])
+
+cur.close()
+=======
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('Drop table if exists counts')
+
+cur.execute('''
+            CREATE TABLE Counts(email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if(len(fname) < 1):
+    fname = 'mbox-short.txt'
+
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From '):
+        continue
+
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    row = cur.fetchone()
+
+    if row is None:
+        cur.execute('INSERT INTO Counts(email, count) VALUES(?, 1)', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+
+    conn.commit()
+
+sql = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+    
+for row in cur.execute(sql):
+    print(str(row[0]), row[1])
+
+cur.close()
+>>>>>>> a3b23b9a0c9e93e0cffe6ee793063319b07d6f24
